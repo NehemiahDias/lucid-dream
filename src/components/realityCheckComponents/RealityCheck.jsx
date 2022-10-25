7f424a03613c168fe57429d3076e89a54faed0a4
SWELLZ <microsoftdavid4@gmail.com>
2022-10-25 16:19:24 -0700

    add beginning form

--- src/components/realityCheckComponents/RealityCheck.jsx
+++ src/components/realityCheckComponents/RealityCheck.jsx
@@ -3,7 +3,17 @@ import './RealityCheck.css';
 
 const RealityCheck = () => {
   return (
-    <div>RealityCheck</div>
+    <section id='reality-check-sec'>
+      <h1>Reality Check</h1>
+      <p>Maybe the most important aspect of Lucid Dream is actually questioning your reality while you're dreaming. That's where reality checks come in! You make it a muscle memory to question the reality around you and once you do it in a dream, you will know you're dreaming. The best way to do this is periodically throughout the day, this feature will send you a notification every hour until a certain time (you can change those settings below).</p>
+      <div>
+        <form className='reality-check-form' onSubmit={e => e.preventDefault()}>
+          <label htmlFor='time'>How often:</label>
+          <input id='time' type='number' />
+          <button type='submit'>+ Create Reality Check</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
